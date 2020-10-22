40dfbe2664c849f0a6fc07225ad6dafc1b917736
KissGuna <2899311889@qq.com>
2020-10-22 15:29:29 +0800

    案例——登陆注册的使用

--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, JsonResponse
+
+
 from users.models import User
 
 # Create your views here.
@@ -10,4 +12,26 @@ def register(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
         User.objects.create(username=username, password=password)
-        return HttpResponse("<h1>注册成功</h1>")
+        return redirect("/login")
+
+
+def login(request):
+    if request.method == "GET":
+        username = request.COOKIES.get("username")
+
+        return render(request, "login.html",context={"username":username})
+
+    else:
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        remember = request.POST.get("remember")
+        try:
+            User.objects.get(username=username, password=password)
+        except:
+            return HttpResponse("<h1>登陆失败</h1>")
+        else:
+            response = JsonResponse({"message": "login success"})
+            if remember == "true":
+                response.set_cookie("username", username,max_age=3600)
+
+        return response
